9b2f61782266422414a9fc54f88554284ff75690
Arun Kumar <arun.kumar.yogi@netcracker.com>
2025-05-21 16:35:34 +0530

    'CLOUDCRM-307618-Contextstorage'

--- src/main/java/org/qubership/integration/platform/catalog/persistence/configs/entity/actionlog/EntityType.java
+++ src/main/java/org/qubership/integration/platform/catalog/persistence/configs/entity/actionlog/EntityType.java
@@ -31,7 +31,8 @@ public enum EntityType {
     MASKED_FIELD,
     CHAINS,
     CHAIN_RUNTIME_PROPERTIES,
-    CONTEXT_SYSTEM,   //removed databases in 24.3
+    DATABASE_SYSTEM,   //removed databases in 24.3
+    CONTEXT_SYSTEM,
     DATABASE_SCRIPT,   //This types remained to avoid error with old actions(in action log) with databases
     SERVICE_DISCOVERY,
     EXTERNAL_SERVICE,
